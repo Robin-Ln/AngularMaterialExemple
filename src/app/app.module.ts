ec440c00515fce79599f0a7ce4f82708d4c1345c
Robin LOUARN <robin.louarn@icloud.com>
2019-05-29 23:52:18 +0200

    Ajout de commentaire sur le module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LayoutModule } from '@angular/cdk/layout';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import {
+  MatInputModule,
+  MatButtonModule,
+  MatSelectModule,
+  MatRadioModule,
+  MatCardModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatGridListModule,
+  MatMenuModule,
+  MatTreeModule
+} from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AddressFormComponent } from './address-form/address-form.component';
-import { MatInputModule, MatButtonModule, MatSelectModule, MatRadioModule, MatCardModule, MatToolbarModule, MatSidenavModule, MatIconModule, MatListModule, MatTableModule, MatPaginatorModule, MatSortModule, MatGridListModule, MatMenuModule, MatTreeModule } from '@angular/material';
-import { ReactiveFormsModule } from '@angular/forms';
 import { NavComponent } from './nav/nav.component';
-import { LayoutModule } from '@angular/cdk/layout';
 import { TableComponent } from './table/table.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { TreeComponent } from './tree/tree.component';
 import { DragDropComponent } from './drag-drop/drag-drop.component';
-import { DragDropModule } from '@angular/cdk/drag-drop';
+
 
 @NgModule({
   declarations: [
+    // Composant principale
     AppComponent,
+    // Composants de test
     AddressFormComponent,
     NavComponent,
     TableComponent,
@@ -27,15 +46,17 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
   ],
   imports: [
     BrowserModule,
+    // Route
     AppRoutingModule,
+    // Animation
     BrowserAnimationsModule,
+    // Matérial
     MatInputModule,
     MatButtonModule,
     MatSelectModule,
     MatRadioModule,
     MatCardModule,
     ReactiveFormsModule,
-    LayoutModule,
     MatToolbarModule,
     MatSidenavModule,
     MatIconModule,
@@ -46,6 +67,9 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
     MatGridListModule,
     MatMenuModule,
     MatTreeModule,
+    // Layout
+    LayoutModule,
+    // Drag & Drop
     DragDropModule
   ],
   providers: [],
