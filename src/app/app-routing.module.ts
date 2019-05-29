fb81fe778e7ddc018addb73a77fac2e89798ff01
Robin LOUARN <robin.louarn@icloud.com>
2019-05-29 21:16:00 +0200

    Modification des routes et de nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { TreeComponent } from './tree/tree.component';
 import { AddressFormComponent } from './address-form/address-form.component';
+import { TableComponent } from './table/table.component';
+import { DragDropComponent } from './drag-drop/drag-drop.component';
 
 const routes: Routes = [
   { path:'dashBoard', component: DashboardComponent },
   { path:'tree', component: TreeComponent },
   { path:'form', component: AddressFormComponent },
+  { path:'table', component: TableComponent },
+  { path:'dragDrop', component: DragDropComponent },
   { path: '**', component: DashboardComponent }
 ];
 
