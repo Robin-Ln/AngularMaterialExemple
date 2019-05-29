eb13fbaae75e9b327fd301c2bf14ec3582a3f98f
Robin LOUARN <robin.louarn@icloud.com>
2019-05-29 21:02:09 +0200

    Affichage des composents

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TreeComponent } from './tree/tree.component';
+import { AddressFormComponent } from './address-form/address-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'dashBoard', component: DashboardComponent },
+  { path:'tree', component: TreeComponent },
+  { path:'form', component: AddressFormComponent },
+  { path: '**', component: DashboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
